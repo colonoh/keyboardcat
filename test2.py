d27f9c1bc09ea7578a958dae5713f3ce4b904e5d
Steve <steve@debian-desktop.ri.cox.net>
2013-04-13 17:39:31 -0400

    remove profiling

--- test2.py
+++ test2.py
@@ -1,20 +1,20 @@
-import encrypt as encrypt
-import cProfile, pstats
-import decrypt3 as decrypt3
+import encrypt
+#import cProfile, pstats
+import decrypt3
 import sys
 
 ciphertext = encrypt.encrypt(sys.argv[1])
 
-
-cProfile.run('wordlist = decrypt3.read_words()', 'rundata')
+#cProfile.run('wordlist = decrypt3.read_words()', 'rundata')
+wordlist = decrypt3.read_words()
 result = decrypt3.decrypt3(ciphertext,wordlist)
 
-# check results
-if sys.argv[1] not in result:
-	print("BAD"*100)
+## check results
+#if sys.argv[1] not in result:
+	#print("BAD"*100)
 
-p = pstats.Stats('rundata')
-p.sort_stats('cumulative').print_stats(10)
+#p = pstats.Stats('rundata')
+#p.sort_stats('cumulative').print_stats(10)
 
 
 #cProfile.run('decrypt3.decrypt3(a,wordlist)', 'rundata')
